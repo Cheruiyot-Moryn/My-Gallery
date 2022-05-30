c52a0f9a8a34cca808091664caea37b75c2d05b8
Moryn <moureen.chepkoech@student.moringaschool.com>
2022-05-30 15:33:24 +0300

    initialize a location class model

--- album/models.py
+++ album/models.py
@@ -13,3 +13,25 @@ class Category(models.Model):
 
     def delete_category(self):
         self.delete()
+
+# Location class
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+
+    @classmethod
+    def get_locations(cls):
+        locations = Location.objects.all()
+        return locations
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
