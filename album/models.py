693e281cafdd91ce67506caa6ed64b3ffd22f633
Moryn <moureen.chepkoech@student.moringaschool.com>
2022-05-30 15:38:00 +0300

    define an image class model

--- album/models.py
+++ album/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from models import CloudinaryField
 
 # Create your models here.
 # Category class 
@@ -34,4 +35,41 @@ class Location(models.Model):
         self.save()
 
     def delete_location(self):
+        self.delete()
+
+# Image class
+class Image(models.Model):
+    # image = models.ImageField(upload_to='images/')
+    image = CloudinaryField('image')
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.ForeignKey( Category, on_delete=models.CASCADE , default='0')
+    location = models.ForeignKey( Location, on_delete=models.CASCADE , default='0')
+
+    @classmethod
+    def filter_by_location(cls, location):
+        location = Image.objects.filter(location__name=location).all()
+        return location
+
+    @classmethod
+    def update_image(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = cls.objects.filter(id=id).all()
+        return image
+
+    @classmethod
+    def search_by_category(cls, category):
+        images = cls.objects.filter(category__name__icontains=category)
+        return images
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
         self.delete()
