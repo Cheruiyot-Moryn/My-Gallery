bee8e7df483cb6aa4d90ad401ea23f5f056f6379
Moryn <moureen.chepkoech@student.moringaschool.com>
2022-05-30 15:23:18 +0300

    create a request function

--- album/views.py
+++ album/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def album(request):
+    images = Image.objects.all()
+    locations = Location.get_locations()
+    print(locations)
+    return render(request, 'album.html', {'images': images[::-1], 'locations': locations})
